d9277645da82bad8e832d107132e4dfad0b12903
Kevin Shi <kshi@qualtrics.com>
2016-07-26 23:09:53 -0700

    First Auth commit, started working on google

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -22,6 +22,92 @@ class Auth extends CI_Controller
 		}
 	}
 
+	function testa() {
+		echo '<pre>';
+		var_dump(password_hash("poopybutthole", PASSWORD_BCRYPT));
+		echo '</pre>';
+		echo '<pre>';
+		var_dump(password_hash("poopybutthole", PASSWORD_BCRYPT));
+		echo '</pre>';
+	}
+
+	function signin() {
+
+		// First determine what type of auth we're using (google, fb, linkedin)
+		//unset($_SESSION['access_token']);
+		require_once APPPATH . 'libraries/google_api/vendor/autoload.php';
+		$scriptURI = base_url('auth/signin');
+		$client = new Google_Client();
+		$client->setAccessType('offline'); // default: offline
+		$client->setApprovalPrompt('force');
+		$client->setApplicationName('Onespace Page Manager');
+		$client->setClientId('449516604176-huv86108vpvqdeafsvckemke9gaq2on2.apps.googleusercontent.com');
+		$client->setClientSecret('O1YSEBCSoXVxC8d4OfHXuowY');
+		$client->setRedirectUri($scriptURI);
+		$client->setScopes('email', 'profile');
+		$client->setDeveloperKey('AIzaSyBNCGK_QeKBH4aTmtWtUtdIQXIbJhvCOXg'); // API key
+
+		//Send Client Request
+		$loginObj = new Google_Service_Oauth2($client);
+
+		//Logout
+		if (isset($_REQUEST['logout'])) {
+		  unset($_SESSION['access_token']);
+		  $client->revokeToken();
+		  header('Location: ' . filter_var($scriptURI, FILTER_SANITIZE_URL)); //redirect user back to page
+		}
+
+		//Authenticate code from Google OAuth Flow
+		//Add Access Token to Session
+		if (isset($_GET['code'])) {
+		  $client->authenticate($_GET['code']);
+		  var_dump($client->getRefreshToken());
+		  $_SESSION['access_token'] = $client->getAccessToken();
+		  //header('Location: ' . filter_var($scriptURI, FILTER_SANITIZE_URL));
+		}
+
+		//Set Access Token to make Request
+		if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+		  $client->setAccessToken($_SESSION['access_token']);
+		}
+
+		//Get User Data from Google Plus
+
+		if ($client->getAccessToken()) {
+			echo '<pre>';
+		  var_dump($client->getAccessToken());
+		  echo '</pre';
+		  $userData = $loginObj->userinfo->get();
+		  echo '<pre>';
+		  var_dump($userData);
+		  echo '</pre';
+		  $_SESSION['access_token'] = $client->getAccessToken();
+		} else {
+		  $authUrl = $client->createAuthUrl();
+		  header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+		}
+
+
+
+	}
+
+	function oauth2callback() {
+		require_once APPPATH . 'libraries/google_api/vendor/autoload.php';
+		$client = new Google_Client();
+		$scriptURI = base_url('auth/oauth2callback');
+		$client->setClientId('449516604176-huv86108vpvqdeafsvckemke9gaq2on2.apps.googleusercontent.com');
+		$client->setClientSecret('O1YSEBCSoXVxC8d4OfHXuowY');
+		$client->setDeveloperKey('AIzaSyBNCGK_QeKBH4aTmtWtUtdIQXIbJhvCOXg'); // API key
+		$client->setRedirectUri($scriptURI);
+		if(isset($_GET['code'])) {
+			$client->authenticate($_GET['code']);
+			echo '<pre>';
+			print_r($client->getAccessToken());
+			echo '</pre>';
+		}
+
+		//var_dump($client->getAccessToken());
+	}
 	/**
 	 * Login user on the site
 	 *
@@ -31,7 +117,7 @@ class Auth extends CI_Controller
 	{
 
 		 //echo $this->input->post('login');
-		 
+
 		if ($this->tank_auth->is_logged_in()) {
 			// logged in
 			redirect('userpage/manage');
@@ -179,11 +265,11 @@ class Auth extends CI_Controller
 
 							$this->_send_email('welcome', $data['email'], $data);
 						}
-						
+
 						$this->tank_auth->login($this->form_validation->set_value('username'), $this->form_validation->set_value('password'), 1, 1, 1);
 						unset($data['password']);
 						redirect('/userpage/manage');;
-						
+
 						//$this->load->view('content/redirect.php', $message);
 						//redirect('/pages/redirect/');
 						//$this->_show_message($this->lang->line('auth_message_registration_completed_2').' '.anchor('/auth/login/', 'Login'));
@@ -332,7 +418,7 @@ class Auth extends CI_Controller
 				$this->load->view('auth/forgot_password_form', $data);
 				$this->load->view('templates/footer.php');
 			}
-			
+
 		}
 	}
 
@@ -433,7 +519,7 @@ class Auth extends CI_Controller
 				$this->load->view('auth/change_password_form', $data);
 				$this->load->view('templates/footer.php');
 			}
-			
+
 		}
 	}
 
