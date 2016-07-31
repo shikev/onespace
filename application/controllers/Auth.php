3c4169652c2383be496345bc3ccee0e772e96179
shikev <shikev@umich.edu>
2016-07-31 05:37:13 -0400

    really added vendor

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -41,11 +41,11 @@ class Auth extends CI_Controller
 	}
 
 	public function googleSignIn() {
-
+		session_start();
 		// First determine what type of auth we're using (google, fb, linkedin)
 		//unset($_SESSION['access_token']);
 		require_once APPPATH . 'libraries/googleAPI/vendor/autoload.php';
-		$scriptURI = base_url('auth/signin');
+		$scriptURI = base_url('manage');
 		$client = new Google_Client();
 		$client->setApplicationName('Onespace Page Manager');
 		$client->setClientId('449516604176-huv86108vpvqdeafsvckemke9gaq2on2.apps.googleusercontent.com');
@@ -53,47 +53,68 @@ class Auth extends CI_Controller
 		$client->setRedirectUri($scriptURI);
 		$client->setScopes('email', 'profile');
 		$client->setDeveloperKey('AIzaSyBNCGK_QeKBH4aTmtWtUtdIQXIbJhvCOXg'); // API key
-
+		$redirect_uri = base_url('auth/googleSignIn');
 		//Send Client Request
 		$loginObj = new Google_Service_Oauth2($client);
 
 		//Authenticate code from Google OAuth Flow
 		//Add Access Token to Session
+		// if (isset($_GET['code'])) {
+		//   $client->authenticate($_GET['code']);
+		//   $accessToken = $client->getAccessToken();
+		//   echo $accessToken;
+		//   if ($accessToken) {
+		//   	$client->setAccessToken($accessToken);
+		//   	$userData = $loginObj->userinfo->get();
+		//   	var_dump($userdata);
+		//   }
+		//   else {
+		//   	// failed redirect to login page
+		//   }
+		// }
+		// else {
+		// 	// failed, redirect to login page
+		// }
+
 		if (isset($_GET['code'])) {
 		  $client->authenticate($_GET['code']);
-		  $accessToken = $client->getAccessToken();
-		  if ($accessToken) {
-		  	$client->setAccessToken($accessToken);
-		  	$userData = $loginObj->userinfo->get();
-		  	var_dump($userdata);
-		  }
-		  else {
-		  	// failed redirect to login page
-		  }
+		  $_SESSION['access_token'] = $client->getAccessToken();
+		  echo $_SESSION['access_token'];
+		  header('Location: ' . filter_var($redirect_uri, FILTER_SANITIZE_URL));
+		  exit;
 		}
-		else {
-			// failed, redirect to login page
+
+		//if we have access_token continue, or else get login URL for user
+		if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+		  $client->setAccessToken($_SESSION['access_token']);
+		    $userData = $loginObj->userinfo->get();
+		  echo '<pre>';
+		  var_dump($userData);
+		  echo '</pre';
+
+		} else {
+		  $authUrl = $client->createAuthUrl();
 		}
 
 		//Set Access Token to make Request
-		if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
-		  $client->setAccessToken($_SESSION['access_token']);
-		}
+		// if (isset($_SESSION['access_token']) && $_SESSION['access_token']) {
+		//   $client->setAccessToken($_SESSION['access_token']);
+		// }
 
-		//Get User Data from Google Plus
+		// //Get User Data from Google Plus
 
-		if ($client->getAccessToken()) {
-			// echo '<pre>';
-		 //  var_dump($client->getAccessToken());
-		 //  echo '</pre';
-		  $userData = $loginObj->userinfo->get();
-		  // echo '<pre>';
-		  // var_dump($userData);
-		  // echo '</pre';
-		} else {
-		  $authUrl = $client->createAuthUrl();
-		  header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
-		}
+		// if ($client->getAccessToken()) {
+		// 	// echo '<pre>';
+		//  //  var_dump($client->getAccessToken());
+		//  //  echo '</pre';
+		//   $userData = $loginObj->userinfo->get();
+		//   // echo '<pre>';
+		//   // var_dump($userData);
+		//   // echo '</pre';
+		// } else {
+		//   $authUrl = $client->createAuthUrl();
+		//   header('Location: ' . filter_var($authUrl, FILTER_SANITIZE_URL));
+		// }
 
 	}
 
